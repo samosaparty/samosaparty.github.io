57c5ff32867ec02c329c8edbc2b1e45a6985262d
khalidengineer <61066087+khalidengineer@users.noreply.github.com>
2026-04-08 12:09:41 +0530

    k

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/samosaparty.github.io",
+  assetPrefix: "/samosaparty.github.io",
   images: {
     unoptimized: true
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- out/_next/static/chunks/app/layout-c313629f28f8eb12.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{76110:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,60936,23)),Promise.resolve().then(l.t.bind(l,45214,23))},45214:()=>{},60936:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}}},e=>{var s=s=>e(e.s=s);e.O(0,[351,130,215,744],()=>s(76110)),_N_E=e.O()}]);
