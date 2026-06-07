7f5bac0009c3eda3e363a03afd0607fa7ff33cdb
khalidengineer <61066087+khalidengineer@users.noreply.github.com>
2026-06-07 18:32:51 +0530

    khalid

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Dashboard } from "@/components/Dashboard";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-4 md:p-6 lg:p-8 max-w-[1920px] mx-auto">
-      <Dashboard />
-    </main>
-  );
-}
